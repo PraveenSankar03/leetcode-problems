ab7591068438c3580e4a8f0eed633ad8af937d6e
Praveen <praveensankar3333@gmail.com>
2026-07-25 13:42:10 +0530

    2108 first plaindrome in string - python and javascript

---
+++ JavaScript/2108_first_palindrome_in_string/solution.js
@@ -0,0 +1,11 @@
+function first_palindrome_in_string(words) {
+    for (let word of words) {
+        if (word == word.split("").reverse().join('')) {
+            return word;
+        }
+    }
+    return "";
+}
+
+const words = ["abc","car","ada","racecar","cool"];
+console.log(first_palindrome_in_string(words))
---
+++ Python/2108_first_palindrome_in_string/solution.py
@@ -0,0 +1,8 @@
+def first_palindrome_in_string(words):
+    for word in words:
+        if word == word[::-1]:
+            return word
+    return ""
+
+words = ["abc","car","ada","racecar","cool"]
+print(first_palindrome_in_string(words))
