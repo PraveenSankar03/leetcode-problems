08d634fedcea7239786101d8cf14f8f5d5738b6e
Praveen <praveensankar3333@gmail.com>
2026-09-13 22:39:37 +0530

    0709 to lower case - python and javascript

---
+++ JavaScript/0709_to_lower_case/solution.js
@@ -0,0 +1,6 @@
+function to_lower_case(string) {
+    return string.toLowerCase();
+}
+
+const string = "Hello";
+console.log(to_lower_case(string));
---
+++ Python/0709_to_lower_case/solution.py
@@ -0,0 +1,5 @@
+def to_lower_case(string):
+    return string.lower()
+
+string = "Hello"
+print(to_lower_case(string))
